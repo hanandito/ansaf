3e8f11799a6262a1634fb7ad93514887f1128f8f
teddypicker <hananditos@gmail.com>
2021-03-18 10:17:29 +0700

    initial commit

--- js/app.js
+++ js/app.js
@@ -22,3 +22,26 @@
      }
    }
  })
+ $('.carousel-ourbusiness').addClass('owl-carousel owl-theme').owlCarousel({
+  stagePadding: 40,
+  loop:false,
+  margin:2,
+  nav:false,
+  items:2,
+  responsive:{
+   0: {
+     items: 1,
+     dots: true,
+     nav: true
+   },
+   600: {
+     items: 1,
+     nav: false
+   },
+   1000: {
+     items: 2,
+     dots: false,
+     nav: false
+   }
+ }
+})
