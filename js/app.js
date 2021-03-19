91ba95fb62f30e0a6fb6d381a19c16f63ecad35c
teddypicker <hananditos@gmail.com>
2021-03-19 15:15:21 +0700

    initial commit

--- js/app.js
+++ js/app.js
@@ -45,3 +45,26 @@
    }
  }
 })
+$('.carousel-milestone').addClass('owl-carousel owl-theme').owlCarousel({
+  stagePadding: 40,
+  loop:false,
+  margin: 17,
+  navText: ["<img src='images/chevron-left-square-08.png'>","<img src='images/chevron-right-square-08.png'>"],
+  items:2,
+  responsive:{
+   0: {
+     items: 1,
+     dots: true,
+     nav: true
+   },
+   600: {
+     items: 1,
+     nav: false
+   },
+   1000: {
+     items: 1,
+     dots: false,
+     nav: true
+   }
+ }
+})
